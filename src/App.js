3ea076045e7691923656f0f075206e789f76e3d7
Andres Parra <alfandres2108@gmail.com>
2023-06-06 09:16:16 -0500

    Primer commit, ToDo en estado de busquedas de todos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
 import logo from './platzi.webp';
-import './App.css';
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList'
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+
+
+const defaultTodos = [
+    { text: 'Tomar agua', completed: false},
+    { text: 'Leer', completed: true},
+    { text: 'Entrenar', completed: false},
+    { text: 'Limpiar', completed: false},
+    { text: 'Usar estados devrivados', completed: true}
+];
 
 function App() {
+
+  const [todos, setTodos]=React.useState(defaultTodos);
+
+  const [searchValue, setSearchValue] = React.useState ('');
+
+  const completedTodos = todos.filter(
+    todo => !!todo.completed
+    ).length;
+  const totalTodos = todos.length;
+
+  const searchTodos = todos.filter(
+    (todo) => {
+    return todo.text.toLowerCase().includes(searchValue.toLocaleLowerCase());
+    }
+  );
+
+  console.log('los usuarios buscan ToDos de: '+ searchValue);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+      <TodoCounter completed={completedTodos} total={totalTodos} />
+      <TodoSearch 
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+
+      />
+      
+      <TodoList>
+        {searchTodos.map( todo => (
+            <TodoItem 
+            key={todo.text} 
+            text={todo.text}
+            completed= {todo.completed} />
+        ))}
+      </TodoList>
+
+      <CreateTodoButton/>
+    </>
   );
 }
 
