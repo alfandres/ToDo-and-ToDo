66734f25802c2e2c372fb4c78821f4d3a0b1f432
Andres Parra <alfandres2108@gmail.com>
2023-06-15 16:02:04 -0500

    Se implemento el crear ToDos

--- src/TodoContext/index.js
+++ src/TodoContext/index.js
@@ -37,6 +37,15 @@ function TodoProvider ( {children} ){
         // return todo.text.toLowerCase().includes(searchValue.toLocaleLowerCase()); 
         }
       );
+
+    const addTodo = (text) => {
+      const newTodos = [...todos]; 
+      newTodos.push({
+        text,
+        completed: false,
+      })
+      saveTodos(newTodos);
+    }
     
     const completeTodo = (text) =>{
       const newTodos = [...todos];
@@ -68,6 +77,7 @@ function TodoProvider ( {children} ){
             deleteTodo,
             openModal,
             setOpenModal,
+            addTodo,
         }}>
             {children}
           </TodoContext.Provider>
--- src/TodoFrom/index.js
+++ src/TodoFrom/index.js
@@ -1,19 +1,43 @@
 import React from "react";
+import { TodoContext } from "../TodoContext";
 import './TodoFrom.css';
+
 
 function TodoFrom( ){
 
+    const {
+        addTodo,
+        setOpenModal,
+    } = React.useContext (TodoContext);
+
+    const [newTodoValue, setNewTodoValue] = React.useState('');
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        addTodo(newTodoValue);
+        setOpenModal(false);
+    };
+
+    const onCancel = () => {
+        setOpenModal(false);
+    };
+
+    const onChange = (event) => {
+        setNewTodoValue(event.target.value);
+    };
+
     return(
-        <form onSubmit = {(event) => {
-             event.preventDefault();
-        }}>
+        <form onSubmit = {onSubmit} >
             <label>Escribe tu nuevo ToDo</label>
             <textarea 
                 placeholder="Comprar frutas"
+                value={newTodoValue}
+                onChange={onChange}
             />
             <div className="TodoFrom-buttonContainer">
             <button 
-                type="submit"
+                type="button"
+                onClick={onCancel}
                 className="TodoFrom-button ">
                     <span className="button_top TodoFrom-button--cancel"> Cancelar</span>
             </button>
